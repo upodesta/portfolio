60cca317b752f8600a9ee7829ffa52308bca266a
upodesta <upodesta@yahoo.com>
2019-11-09 01:10:33 +0600

    biru

---
+++ js/agency.js
@@ -0,0 +1,2 @@
+$(function(){$('a.page-scroll').bind('click',function(event){var $anchor=$(this);$('html, body').stop().animate({scrollTop:$($anchor.attr('href')).offset().top},450,'easeInOutCirc');event.preventDefault();});});$('body').scrollspy({target:'.navbar-fixed-top'})
+$('.navbar-collapse ul li a').click(function(){$('.navbar-toggle:visible').click();});$('div.modal').on('show.bs.modal',function(){var modal=this;var hash=modal.id;window.location.hash=hash;window.onhashchange=function(){if(!location.hash){$(modal).modal('hide');}}});
